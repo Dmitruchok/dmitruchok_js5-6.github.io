65545087cf3266fb526aa673ce3a5f674ec0ef68
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-21 14:12:28 +0300

    5-6

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 count_time.addEventListener( 'click', start_count );
-
+clear.addEventListener( 'click', stop_count );
+count_pause.addEventListener( 'click', pause );
 
 var count = 0,
   timerId,
@@ -10,16 +11,33 @@ function start_count() {
       count++;
       var milisec = count%1000;
       for_milisec.innerHTML = milisec;
-      var sec = Math.floor( count/1000 )%60;
+      var sec = Math.floor(count/1000)%60;
       if (sec<10) {sec = "0"+sec;}
       var min = Math.floor(count/60000)%60;
-      var hours = Math.floor(count/360000)%24;
+      if (min<10) {min = "0"+min;}
+      var hours = Math.floor(count/3600000)%24;
+      if (hours<10) {hours = "0"+hours;}
       var str = hours+":"+min+":"+sec;
       for_time.innerHTML=str;
     }, 1);
       go=true;
     }
+}
+
+function pause() {
+  clearInterval(timerId);
+  go = false;
+}
+
+
+function stop_count() {
+  clearInterval(timerId);
+  go = false;
+  count = 0;
+  for_time.innerHTML = ('00:00:00');
+  for_milisec.innerHTML = ('00');
 };
+
 /*start.addEventListener("click",start_click);
 pause.addEventListener("click",pause_click);
 stp.addEventListener("click",stp_click);
