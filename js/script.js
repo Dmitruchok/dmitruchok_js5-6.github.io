3da1477dfcc86e5db8ba8f7d584a1be1847743c4
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-26 00:15:19 +0300

    5-6

--- js/script.js
+++ js/script.js
@@ -1,42 +1,4 @@
-/*var count = 0,
-  timerId,
-  go = false,
-  min = 0,
-  sek = 0;
-  milisec = 0;
-  //start_time = for_time.innerHTML = ('00:00:00'),
-  //start_milisec = for_milisec.innerHTML = ('00');
-function start_count() {
-  if (go == false ) {
-    timerId = setInterval(function(){
-      milisec++;
-      if (milisec == 1000) {
-        milisec = 0;
-        sek++;
-      }
-if (sek==60) {
-  sek = 0;
-  min++;
-}
-var str = min+":"+sek+":"+milisec;
-for_time.innerHTML=str;*/
-
-/*function startTime() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var milisec = date.getMilliseconds();
-    if (hours < 10) hours = "0" + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-    if (milisec == 1000) milisec = "0" + milisec;
-    for_time.innerHTML = hours + ":" + minutes + ":" + seconds + ":" + milisec;
-    setTimeout(startTime, 1000);
-  }*/
-
-
-count_time.addEventListener( 'click', start_count );
+/*count_time.addEventListener( 'click', start_count );
 clear.addEventListener( 'click', stop_count );
 count_pause.addEventListener( 'click', pause );
 
@@ -54,6 +16,52 @@ function start_count() {
       var sec = Math.floor( count*4/1000 )%60;
       var min = Math.floor( count*4/60000 )%60;
       var hours = Math.floor( count*4/3600000 )%24;
+      if ( milisec<10 ) {
+         milisec = '00' + milisec }
+      else if (milisec<100){
+        milisec = '0' + milisec
+      };
+      if ( sec<10 ) { sec = '0' + sec };
+      if  (min<10 ) { min = '0' + min };
+      if ( hours<10 ) { hours = '0' + hours };
+      var str = hours + ":" + min+ ":" + sec;
+      for_time.innerHTML=str;
+      for_milisec.innerHTML = milisec;
+    }, 1);
+      go=true;
+    }
+}
+
+function pause() {
+  clearInterval( timerId );
+  go = false;
+}
+
+
+function stop_count() {
+  clearInterval( timerId );
+  go = false;
+  count = 0;
+  for_time.innerHTML = start_time ;
+  for_milisec.innerHTML = start_milisec;
+};*/
+
+count_time.addEventListener( 'click', start_count );
+clear.addEventListener( 'click', stop_count );
+count_pause.addEventListener( 'click', pause );
+var this_date = new Date(),
+  timerId,
+  go = false,
+  start_time = for_time.innerHTML = ( '00:00:00' ),
+  start_milisec = for_milisec.innerHTML = ( '000' );
+function start_count() {
+  if (go == false ) {
+    timerId = setInterval(function(){
+      var new_date = new Date() - this_date;
+      var milisec = new_date%1000;
+      var sec = Math.floor( new_date/1000 )%60;
+      var min = Math.floor( new_date/60000 )%60;
+      var hours = Math.floor( new_date/3600000 )%24;
       if ( milisec<10 ) {
          milisec = '00' + milisec }
       else if (milisec<100){
