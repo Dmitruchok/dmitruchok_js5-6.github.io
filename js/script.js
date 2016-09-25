3ee80fb790144c58317b927c84058f41f9325f0a
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-25 23:57:41 +0300

    5

--- js/script.js
+++ js/script.js
@@ -43,35 +43,41 @@ count_pause.addEventListener( 'click', pause );
 var count = 0,
   timerId,
   go = false,
-  start_time = for_time.innerHTML = ('00:00:00'),
-  start_milisec = for_milisec.innerHTML = ('00');
+  start_time = for_time.innerHTML = ( '00:00:00' ),
+  start_milisec = for_milisec.innerHTML = ( '000' );
 function start_count() {
   if (go == false ) {
     timerId = setInterval(function(){
       count++;
-      var milisec = count%1000;
-      for_milisec.innerHTML = milisec;
-      var sec = Math.floor(count/1000)%60;
-      if (sec<10) {sec = "0"+sec;}
-      var min = Math.floor(count/60000)%60;
-      if (min<10) {min = "0"+min;}
-      var hours = Math.floor(count/3600000)%24;
-      if (hours<10) {hours = "0"+hours;}
-      var str = hours+":"+min+":"+sec;
+      var milisec = (count*4)%1000;
+
+      var sec = Math.floor( count*4/1000 )%60;
+      var min = Math.floor( count*4/60000 )%60;
+      var hours = Math.floor( count*4/3600000 )%24;
+      if ( milisec<10 ) {
+         milisec = '00' + milisec }
+      else if (milisec<100){
+        milisec = '0' + milisec
+      };
+      if ( sec<10 ) { sec = '0' + sec };
+      if  (min<10 ) { min = '0' + min };
+      if ( hours<10 ) { hours = '0' + hours };
+      var str = hours + ":" + min+ ":" + sec;
       for_time.innerHTML=str;
+      for_milisec.innerHTML = milisec;
     }, 1);
       go=true;
     }
 }
 
 function pause() {
-  clearInterval(timerId);
+  clearInterval( timerId );
   go = false;
 }
 
 
 function stop_count() {
-  clearInterval(timerId);
+  clearInterval( timerId );
   go = false;
   count = 0;
   for_time.innerHTML = start_time ;
